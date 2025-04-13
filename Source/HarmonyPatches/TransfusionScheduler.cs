1eeefc99a1976848366a0d3527163d1f44515d40
Daniel Oakey <doakey3@gmail.com>
2025-04-13 13:28:48 +0900

    change from checking whether transfusion should occur every tick to every 4 seconds (CompTickRare)

--- Source/HarmonyPatches/TransfusionScheduler.cs
+++ Source/HarmonyPatches/TransfusionScheduler.cs
@@ -1,36 +1,32 @@
-using System.Collections.Generic;
-using HarmonyLib;
 using RimWorld;
 using Verse;
 
-namespace Pharmacist.HarmonyPatches {
-    [HarmonyPatch(typeof(Pawn_HealthTracker), nameof(Pawn_HealthTracker.HealthTick))]
-    public static class Patch_Pawn_HealthTick_TransfusionScheduler {
-        public static void Postfix(Pawn_HealthTracker __instance) {
-            Pawn pawn = Traverse.Create(__instance).Field("pawn").GetValue<Pawn>();
-            if (pawn == null || !pawn.Spawned || pawn.Map == null || pawn.Dead)
-                return;
+namespace Pharmacist
+{
+    public class CompAutoTransfuse : ThingComp
+    {
+        public override void CompTickRare()
+        {
+            base.CompTickRare();
+
+            if (parent is not Pawn pawn) return;
+            if (!pawn.Spawned || pawn.Map == null || pawn.Dead) return;
 
             Hediff bloodLoss = pawn.health.hediffSet.GetFirstHediffOfDef(HediffDefOf.BloodLoss);
             if (bloodLoss == null || bloodLoss.Severity < PharmacistSettings.hemogenThreshold)
                 return;
 
-            bool enableTransfusion = false;
-            if (pawn.IsColonist) {
-                enableTransfusion = PharmacistSettings.autoTransfuseColonists;
-            } else if (pawn.IsPrisoner) {
-                enableTransfusion = PharmacistSettings.autoTransfusePrisoners;
-            } else if (pawn.HostFaction != null && !pawn.IsPrisoner) {
-                enableTransfusion = PharmacistSettings.autoTransfuseGuests;
-            }
+            bool enableTransfusion = pawn.IsColonist ? PharmacistSettings.autoTransfuseColonists :
+                                   pawn.IsPrisoner ? PharmacistSettings.autoTransfusePrisoners :
+                                   pawn.HostFaction != null ? PharmacistSettings.autoTransfuseGuests : false;
 
             if (!enableTransfusion)
                 return;
 
-            foreach (var existingBill in pawn.BillStack.Bills) {
-                if (existingBill.recipe != null && existingBill.recipe.defName == "BloodTransfusion") {
+            foreach (var existingBill in pawn.BillStack.Bills)
+            {
+                if (existingBill.recipe != null && existingBill.recipe.defName == "BloodTransfusion")
                     return;
-                }
             }
 
             RecipeDef recipe = DefDatabase<RecipeDef>.GetNamedSilentFail("BloodTransfusion");
@@ -39,6 +35,14 @@ namespace Pharmacist.HarmonyPatches {
 
             Bill_Medical newBill = new Bill_Medical(recipe, null);
             pawn.BillStack.AddBill(newBill);
+        }
+    }
+
+    public class CompProperties_AutoTransfuse : CompProperties
+    {
+        public CompProperties_AutoTransfuse()
+        {
+            compClass = typeof(CompAutoTransfuse);
         }
     }
 }
